f6ba88ab392a2cce31fdd78119aba6d63389d0ed
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2023-04-17 16:05:18 +0300

    modified:   utils/catchAsync.js

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,3 +1,13 @@
-module.exports = fn => (req, res, next) => {
+const catchAsync = fn => (req, res, next) => {
 	fn(req, res, next).catch(error => next(error));
+};
+
+const asyncWrapper = controller => {
+	return (req, res, next) => {
+		controller(req, res).catch(next);
+	};
+};
+
+module.exports = {
+	catchAsync,
 };
